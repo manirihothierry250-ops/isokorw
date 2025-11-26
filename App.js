bcb8fbb6d7d00bbcf8b52d7f7b78a7574045a3fb
manirihothierry250-ops <manirihothierry250@gmail.com>
2025-11-26 21:15:11 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,1 @@
+export default function App(){return (<div>Isoko App Placeholder</div>);}
---
+++ index.js
@@ -0,0 +1,1 @@
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; const r=ReactDOM.createRoot(document.getElementById('root')); r.render(<App />);
